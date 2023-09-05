67c451aabf0332a6613ed6f3e590c639f428511d
Alkesh Nikalje <alkeshnikalje@gmail.com>
2023-09-05 15:00:27 +0530

    prepared the code for deployment

--- server.js
+++ server.js
@@ -12,11 +12,27 @@ const premiumRouter = require('./routes/premiumFeature');
 const forgotPassRouter = require('./routes/forgotpass');
 const Downloads = require('./models/downloadfile');
 const fileRouter = require('./routes/files');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+}
+));
+
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, "access.log"),
+    { flags: "a" }
+  );
+
+  app.use(morgan('combined', {stream: accessLogStream}));
 
 app.use('/api/user',userRounter);
 app.use('/api/user/expenses', expenseRouter);
@@ -24,6 +40,7 @@ app.use('/api', purchaseRouter);
 app.use('/api/user',premiumRouter); 
 app.use('/api/user', forgotPassRouter);
 app.use('/api/user/expenses', fileRouter);
+
 
 User.hasMany(Expense);
 
